91e52a846405119c96a387ebf1289a27ba9dc495
Samuel-Tim <70382402+Samuel-Tim@users.noreply.github.com>
2021-03-11 12:08:20 -0500

    carroussel

---
+++ scripts/discographie.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+    autoplay:{delay: 1500},
+     loop: true,
+     speed: 400,
+     effect:'cube',
+     freeMode: true,
+     
+     cubeEffect: {
+       slideShadows: true,
+     }
+     
+    });
+   
