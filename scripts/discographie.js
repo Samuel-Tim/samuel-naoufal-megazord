aa82a315ea713a42984fd9d8a5e2a864fc81d32c
Samuel-Tim <70382402+Samuel-Tim@users.noreply.github.com>
2021-03-11 12:21:49 -0500

    carroussel_final

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
-    autoplay:{delay: 1500},
+    autoplay:{delay: 2000},
+    direction: 'vertical',
      loop: true,
      speed: 400,
      effect:'cube',
