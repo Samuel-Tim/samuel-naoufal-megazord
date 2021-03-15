7600329067f099b82c304e62f6b07f3b905c5a3a
Naoufal Bensaiad <nbensaiad@hotmail.com>
2021-03-14 22:17:59 -0400

    CN26

---
+++ scripts/acceuil.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spacebetween:100,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        bulletActiveClass: 'bullet-active',
+        },
+  });
