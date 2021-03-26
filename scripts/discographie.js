969cc2eb2892937249c2b52b2ffdb279b88e9701
Samuel-Tim <70382402+Samuel-Tim@users.noreply.github.com>
2021-03-26 17:13:02 -0400

    section-animation

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -12,24 +12,38 @@ const swiper = new Swiper('.swiper-container', {
      
     });
 
+  gsap.registerPlugin(ScrollTrigger);
+
 
   const youtube = document.querySelector('.textytb')
   const albums = document.querySelector('.albums')
-
-  youtube.ScrollTrigger(function() {
-    gsap.timeline( scrollTrigger: {
-      start: 'bottom bottom',
-      trigger: section,
+  const pied = document.querySelector('.pied')
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: '.textytb',
+        toggleActions: 'restart complete reverse reset',
+      } 
+    })
       .from('.textytb',{y: '100'})
-      .from('.youtube', {y:'100', opacity: '0', delay: '1'})
-       })
-    });
+      .from('.youtube', {y:'100', opacity: '0', delay: '1'});
 
-  albums.ScrollTrigger(function(){
-    gsap.timeline()
-    .from('.albums', {y: '100', opacity: '-5'})
-    .from('.btn_album', {y: '100', opacity: '-100', delay: '1'})
-  })
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: '.btn_album',
+        toggleActions: 'restart complete reverse reset',
+      }
+    })
+    .from('.albums', {y: '100', opacity: '0'})
+    .from('.btn_album', {y: '100', opacity: '-100', delay: '1',});
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: '.pied',
+        toggleActions: 'restart complete reverse reset',
+      }
+    })
+    .from('.pied', {y:'100', opacity: '0',});
+
 
 
  
