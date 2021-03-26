1c457c83b4eda26a7b8aa1e3617a8d6cadcaf0a2
Samuel-Tim <70382402+Samuel-Tim@users.noreply.github.com>
2021-03-26 11:47:31 -0400

    animation-section

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -11,4 +11,34 @@ const swiper = new Swiper('.swiper-container', {
      }
      
     });
-   
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const bannerList =
+
+document.querySelectorAll('banner');
+
+bannerList.forEach(banner => {
+  const albums = banner.querySelector('albums');
+  const youtube = banner.querySelector('youtube');
+
+
+  gsap.timeline({
+    scrollTrigger: {
+      start:'bottom bottom',
+      trigger: banner,
+    }
+  })
+  .from(albums, {
+    y:100,
+    opacity:0
+  })
+  .from(youtube, {
+    opacity: 0
+  })
+});
+
+
+
+
