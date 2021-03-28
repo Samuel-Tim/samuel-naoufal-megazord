9fe2b8b159e477768dbe42202db7a7fd57b4dc45
Naoufal Bensaiad <nbensaiad@hotmail.com>
2021-03-28 19:24:57 -0400

    CN2

--- scripts/acceuil.js
+++ scripts/acceuil.js
@@ -10,3 +10,25 @@ var swiper = new Swiper('.swiper-container', {
 
 gsap.registerPlugin(ScrollTrigger);
 
+const imgpromo = document.querySelector('.images-promotionelles');
+const grp = document.querySelector('.membres-du-grp');
+const yt = document.querySelector('.yt');
+
+yt.addEventListener('click', function() {
+gsap.timeline()
+    .from('.titre-yt',{x: '-100%'})
+    .from('.youtube', {opacity: '0', y: '100%'})
+});
+
+grp.addEventListener('click', function() {
+    gsap.timeline()
+        .from('.card', {opacity: '0'}) 
+        .from('.card-body',{fontSize: '30px'} )
+        .from('.c_img',{scale:'0.5'} )
+    });
+
+imgpromo.addEventListener('click', function() {
+        gsap.timeline()
+            .from('.promo_1',{skewx: '10deg'})
+            .from('.swiper-container', {opacity: '0'})
+        });
