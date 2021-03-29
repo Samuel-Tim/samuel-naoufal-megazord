6b8718676a7be799c4262679d4f110d491471441
Naoufal Bensaiad <nbensaiad@hotmail.com>
2021-03-28 20:33:23 -0400

    CN3

--- scripts/acceuil.js
+++ scripts/acceuil.js
@@ -14,21 +14,32 @@ const imgpromo = document.querySelector('.images-promotionelles');
 const grp = document.querySelector('.membres-du-grp');
 const yt = document.querySelector('.yt');
 
-yt.addEventListener('click', function() {
-gsap.timeline()
+gsap.timeline({
+        scrollTrigger: {
+          trigger: '.yt',
+          toggleActions: 'restart complete reverse reset',
+        } 
+      })
     .from('.titre-yt',{x: '-100%'})
-    .from('.youtube', {opacity: '0', y: '100%'})
-});
+    .from('.youtube', {opacity: '0', y: '100%', delay:'1'});
 
-grp.addEventListener('click', function() {
-    gsap.timeline()
+    gsap.timeline({
+        scrollTrigger: {
+        trigger: '.membres-du-grp',
+        toggleActions: 'restart complete reverse reset',
+          }
+    })
         .from('.card', {opacity: '0'}) 
         .from('.card-body',{fontSize: '30px'} )
-        .from('.c_img',{scale:'0.5'} )
-    });
+        .from('.c_img',{scale:'0.5'} );
 
-imgpromo.addEventListener('click', function() {
-        gsap.timeline()
-            .from('.promo_1',{skewx: '10deg'})
-            .from('.swiper-container', {opacity: '0'})
-        });
+    gsap.timeline({
+        scrollTrigger: {
+        trigger: '.Images-promotionnelles',
+        toggleActions: 'restart complete reverse reset',
+        }
+    })
+            
+        .from('.Images-promotionnelles', {opacity: '0'})
+        .from('.swiper-container', {x: '100%'})
+        .from('.swiper-pagination', {rotate:'90deg' , delay:'0.5'});
